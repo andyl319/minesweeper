538b6d552d304124e704d6076819697b8161f782
App Academy Student <student@appacademy.io>
2016-11-17 18:01:01 -0800

    Implement drop_bombs and render methods

--- board.rb
+++ board.rb
@@ -5,7 +5,7 @@ class Board
 
   def initialize
     @grid = Array.new(9) { Array.new(9) {Tile.new(self)} }
-    populate(@grid)
+    drop_bombs
   end
 
   def random_pos
@@ -13,20 +13,32 @@ class Board
   end
 
   def drop_bombs
-    30.times do
+    20.times do
       pos = random_pos
       pos = random_pos until self[pos].has_bomb == false
       self[pos].has_bomb = true
+      update_bomb_count(pos)
     end
   end
 
-  def populate(grid)
-    @grid.each do |row|
-      row.each do |tile|
-        
+  def neighbor_positions(pos)
+    row, col = pos
+    result = []
+    (row - 1..row + 1).each do |i|
+      (col-1..col + 1).each do |j|
+        neighbor_pos = [i, j]
+        if neighbor_pos.all? { |e| (0..8).cover?(e) }
+          result << neighbor_pos
+        end
       end
     end
+    result
+  end
 
+  def update_bomb_count(pos)
+    neighbor_positions(pos).each do |neighbor|
+      self[neighbor].neighbor_bombs += 1
+    end
   end
 
   def [](pos)
@@ -39,5 +51,12 @@ class Board
     tile = grid[x][y]
     tile.status = status
   end
+
+  def render
+  puts "  "+ (0..8).to_a.join(" ")
+  grid.each_with_index do |row, i|
+    puts "#{i} #{row.map(&:render).join(" ")}"
+  end
+end
 
 end
--- tile.rb
+++ tile.rb
@@ -1,13 +1,21 @@
 class Tile
-  attr_accessor :status, :has_bomb
-  attr_reader :board
+  attr_accessor :status, :has_bomb, :neighbor_bombs
+  attr_reader :board, :revealed, :flagged
 
   def initialize(board)
-
     @board = board
     @has_bomb = false
-    @status = "not_exp"
+    @revealed = false
+    @flagged = false
+    @neighbor_bombs = 0
   end
 
+  def render
+    if @revealed == false
+      @flagged == true ? "F" : "*"
+    else
+      @neighbor_bombs == 0 ? "_" : @neighbor_bombs.to_s
+    end
+  end
 
 end
